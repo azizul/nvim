8ff1215de4a43b389ee01c1ebf6bb636f98f54bc
Azizul Rahman Azizan <Azizul.Rahman.Azizan@gmail.com>
2022-08-30 18:06:37 +0800

    remove warning messages for lsp resolved_capabilities

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -82,20 +82,19 @@ end
 
 M.on_attach = function(client, bufnr)
 	if client.name == "tsserver" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 
 	if client.name == "sumneko_lua" then
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 	end
 
 	if client.name == "jdtls" then
 		require("jdtls").setup_dap({ hotcodereplace = "auto" })
 		require("jdtls.dap").setup_dap_main_class_configs()
-		client.resolved_capabilities.document_formatting = false
+		client.server_capabilities.document_formatting = false
 		vim.lsp.codelens.refresh()
 	end
-
 	lsp_keymaps(bufnr)
 	lsp_highlight_document(client)
 end
