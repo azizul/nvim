38b912dd1779dff92ec8db92e168b2176b9baba9
Azizul Rahman Azizan <Azizul.Rahman.Azizan@gmail.com>
2022-10-04 18:29:57 +0800

    Show full path of file

--- lua/user/utility/nvim.lua
+++ lua/user/utility/nvim.lua
@@ -3,8 +3,8 @@ local M = {}
 vim.api.nvim_set_hl(0, 'WinBarPath', { bg = '#080808', fg = '#80a0ff' })
 vim.api.nvim_set_hl(0, 'WinBarModified', { bg = '#080808', fg = '#ff5454' })
 
-function M.eval()
-	local file_path = vim.api.nvim_eval_statusline("%f", {}).str
+function M.winbar_config()
+	local file_path = vim.api.nvim_eval_statusline("%F", {}).str
 	local modified = vim.api.nvim_eval_statusline("%M", {}).str == '+' and ' ⊕' or ''
 
     file_path = file_path:gsub('/',' ➤ ')
